5bf4fcde4fca08576af32c635a609a6f5c8487a4
wanglaibing <WLBSky@outlook.com>
2022-06-17 11:44:55 +0800

    common调整redis配置

--- src/main/java/com/dazhi100/common/component/infrastructure/RedisConfig.java
+++ src/main/java/com/dazhi100/common/component/infrastructure/RedisConfig.java
@@ -1,9 +1,8 @@
 package com.dazhi100.common.component.infrastructure;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -13,8 +12,9 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @Configuration
 public class RedisConfig {
 
+    @SuppressWarnings("all")
     @Bean
-    public RedisTemplate<String, String> redisTemplate(@Autowired LettuceConnectionFactory factory) {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         StringRedisTemplate stringRedisSerializer = new StringRedisTemplate(factory);
         return stringRedisSerializer;
     }
