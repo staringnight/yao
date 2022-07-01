4227d66c9c12d85087e96c9557767194d120f7ff
wanglaibing <WLBSky@outlook.com>
2022-07-01 08:55:56 +0800

    错误引用

--- src/main/java/com/dazhi100/common/utils/interceptor/HttpLoggingInterceptor.java
+++ src/main/java/com/dazhi100/common/utils/interceptor/HttpLoggingInterceptor.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
@@ -13,9 +12,9 @@ import java.io.IOException;
  */
 @Slf4j
 public class HttpLoggingInterceptor implements Interceptor {
-    @NotNull
+
     @Override
-    public Response intercept(@NotNull Chain chain) throws IOException {
+    public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
         long startTime = System.currentTimeMillis();
         Response proceed = chain.proceed(request);
