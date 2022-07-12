90146e0c020e49faac121674e3c783f27973eb79
wanglaibing <WLBSky@outlook.com>
2022-07-12 18:24:09 +0800

    Snowflake

--- src/main/java/com/pokeya/yao/component/infrastructure/SnowflakeConfig.java
+++ src/main/java/com/pokeya/yao/component/infrastructure/SnowflakeConfig.java
@@ -1,18 +1,26 @@
 package com.pokeya.yao.component.infrastructure;
 
 import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.net.NetUtil;
+import cn.hutool.core.util.HashUtil;
 import cn.hutool.core.util.IdUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
+@Slf4j
 @Configuration
 public class SnowflakeConfig {
 
+    private static final String workIdStr = NetUtil.getLocalhost().getHostAddress() + System.currentTimeMillis();
+
     @Lazy
     @Bean
-    public Snowflake snowflake(@Value("${snow.wokerId}") int wokerId, @Value("${snow.datacenterId}") int datacenterId) {
+    public Snowflake snowflake(@Value("${server.port}") int datacenterId) {
+        Long wokerId = HashUtil.mixHash(workIdStr);
+        log.info("SnowflakeConfig:{}:{}", wokerId, datacenterId);
         return IdUtil.getSnowflake(wokerId, datacenterId);
     }
 
